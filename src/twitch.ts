d671cea8098d16a13922e4b7696ff35478ca3c5b
yoyozbi <yohan.zbinden@gmail.com>
2020-09-07 21:58:42 +0200

    added docker-compose to use mongodb and now in typescript

--- src/twitch.ts
+++ src/twitch.ts
@@ -1,22 +1,28 @@
 import * as cron from 'node-cron'
+import { CommandoClient } from 'discord.js-commando'
 import { Requests } from './req'
 export class Twitch {
     private streamers: String[] = []
+    private client: CommandoClient | undefined
     private task = cron.schedule(
         '* * * * * *',
         () => {
             Requests.getUsersLive(this.streamers, function (
                 streamers: String[]
             ) {
-                if (streamers.length > 0) console.log(streamers)
+                if (streamers.length > 0) {
+                }
             })
         },
         {
             scheduled: false,
         }
     )
-    constructor(streamers: String[]) {
+    constructor(streamers: String[], client: CommandoClient) {
         if (streamers) this.streamers = streamers
+        this.client = client
     }
-    beginRequests() {}
+    beginRequests() {
+        this.task.start()
+    }
 }
